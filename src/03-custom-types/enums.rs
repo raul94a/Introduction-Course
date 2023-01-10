135b283fdd3d46a934192d2ff339a0a1e97d38f1
raul94a <raul200794@gmail.com>
2023-01-10 20:46:52 +0100

    Variable bindings: mutability

---
+++ src/03-custom-types/enums.rs
@@ -0,0 +1,75 @@
+// Create an `enum` to classify a web event. Note how both
+// names and type information together specify the variant:
+// `PageLoad != PageUnload` and `KeyPress(char) != Paste(String)`.
+// Each is different and independent.
+enum WebEvent {
+    // An `enum` may either be `unit-like`,
+    PageLoad,
+    PageUnload,
+    // like tuple structs,
+    KeyPress(char),
+    Paste(String),
+    // or c-like structures.
+    Click { x: i64, y: i64 },
+}
+
+// A function which takes a `WebEvent` enum as an argument and
+// returns nothing.
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // Destructure `c` from inside the `enum`.
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // Destructure `Click` into `x` and `y`.
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        },
+    }
+}
+
+//You can make type aliases
+
+enum InternetConnection {
+    On,
+    Off,
+    Awaiting, 
+}
+
+type InternetConnectionType = InternetConnection;
+
+
+//example where can use type aliases
+impl InternetConnectionType {
+    fn connected(&self) -> bool {
+        match self{
+            Self::On => true,
+            Self::Off => false,
+            Self::Awaiting => false
+        }
+    }
+}
+
+fn get_internet_connection_on(){
+    let on = InternetConnectionType::Awaiting;
+    let isConnected = on.connected();
+    println!("Internet is connected: {}",isConnected);
+}
+
+fn main() {
+    get_internet_connection_on();
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` creates an owned `String` from a string slice.
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y: 80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+}
+
